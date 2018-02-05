9e3323dbb97a1907713df0e7d4cef05ece08c1d4
vk <kaligularnd@gmail.com>
2018-02-05 22:11:36 +0300

    hackerrank algorithms #7

---
+++ src/main/java/vk/dev/trials/hackerrank/algorithms/Task7.java
@@ -0,0 +1,33 @@
+package vk.dev.trials.hackerrank.algorithms;
+
+import java.util.Scanner;
+
+/**
+ * Task7.
+ *
+ * @author vk
+ * @since 05.02.2018
+ */
+public class Task7 {
+
+    // solution goes here
+    static void staircase(int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                char c = j < n - i - 1 ? ' ' : '#';
+                sb.append(c);
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        staircase(n);
+        in.close();
+    }
+
+}
