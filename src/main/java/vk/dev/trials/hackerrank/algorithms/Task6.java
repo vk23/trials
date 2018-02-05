2e8ba6796804496e36d81bbd4976857d2ab979b9
vk <kaligularnd@gmail.com>
2018-02-05 20:48:29 +0300

    hackerrank algorithms 6

--- src/main/java/vk/dev/trials/hackerrank/algorithms/Task6.java
+++ src/main/java/vk/dev/trials/hackerrank/algorithms/Task6.java
@@ -3,7 +3,7 @@ package vk.dev.trials.hackerrank.algorithms;
 import java.util.Scanner;
 
 /**
- * Task5.
+ * Task6.
  *
  * @author vk
  * @since 05.02.2018
@@ -12,14 +12,29 @@ public class Task6 {
 
     // solution goes here
     static void plusMinus(int[] arr) {
+        int n = arr.length;
+        int[] sums = new int[3];
+        for (int i = 0; i < n; i++) {
+            int x = arr[i];
+            if (x > 0) {
+                sums[0]++;
+            } else if (x < 0) {
+                sums[1]++;
+            } else {
+                sums[2]++;
+            }
+        }
 
+        for (int i = 0; i < sums.length; i++) {
+            System.out.format("%f%n", sums[i]*1d/n);
+        }
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] arr = new int[n];
-        for(int arr_i = 0; arr_i < n; arr_i++){
+        for (int arr_i = 0; arr_i < n; arr_i++) {
             arr[arr_i] = in.nextInt();
         }
         plusMinus(arr);
