d6cfd74cfd2299ad47d4c968ca1fdcb5724e9367
vk <kaligularnd@gmail.com>
2018-02-14 23:02:45 +0300

    hackerrank Algorithms strings: Easy3 SockMerchant.

---
+++ src/main/java/vk/dev/trials/hackerrank/algorithms/implementation/Easy3_SockMerchant.java
@@ -0,0 +1,46 @@
+package vk.dev.trials.hackerrank.algorithms.implementation;
+
+import java.util.Scanner;
+
+/**
+ * Easy1_Kangaroo https://www.hackerrank.com/challenges/kangaroo.
+ *
+ * @author vk
+ * @since 06.02.2018
+ */
+public class Easy3_SockMerchant {
+
+    static int sockMerchant(int n, int[] ar) {
+
+        int pairCount = 0;
+        for (int i = 0; i < ar.length; i++) {
+            for (int j = 0; j < ar.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (ar[i] == Integer.MIN_VALUE) {
+                    continue;
+                }
+                if (ar[i] == ar[j]) {
+                    pairCount++;
+                    ar[i] = Integer.MIN_VALUE;
+                    ar[j] = Integer.MIN_VALUE;
+                    break;
+                }
+            }
+        }
+
+        return pairCount;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = sockMerchant(n, ar);
+        System.out.println(result);
+    }
+}
