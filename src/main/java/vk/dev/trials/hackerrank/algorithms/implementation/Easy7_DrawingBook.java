1bea390b2ef0b7448c6e44cd317fb93bd21b0a7f
vk23 <kaligularnd@gmail.com>
2018-07-09 22:02:18 +0300

    hackerrank Algorithms implemenation: Easy7 Drawing book.

---
+++ src/main/java/vk/dev/trials/hackerrank/algorithms/implementation/Easy7_DrawingBook.java
@@ -0,0 +1,38 @@
+package vk.dev.trials.hackerrank.algorithms.implementation;
+
+import java.util.Scanner;
+
+/**
+ * Easy7_DrawingBook.
+ *
+ * @author vk
+ * @since 07.09.2018
+ */
+public class Easy7_DrawingBook {
+
+    /*
+     * Complete the pageCount function below.
+     */
+    static int pageCount(int n, int p) {
+        boolean firstHalf = n >> 1 >= p;
+//        System.out.printf("%d >= %d - %b%n", n>>1, p, firstHalf);
+        if (firstHalf) {
+            return p >> 1;
+        } else {
+            int offset = n % 2 == 0 ? 1 : 0;
+            return (n - p + offset) >> 1;
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        int p = scanner.nextInt();
+
+        int result = pageCount(n, p);
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
