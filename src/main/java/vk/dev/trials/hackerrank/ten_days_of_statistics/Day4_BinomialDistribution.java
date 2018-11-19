2b578dc1c8a71aedfefb8670874889490953df5a
Vladimir Kuragin <vladimir_kuragin@epam.com>
2018-11-19 14:01:55 +0300

    hackerrank: 10 days of statistics: Day4_BinomialDistribution

---
+++ src/main/java/vk/dev/trials/hackerrank/ten_days_of_statistics/Day4_BinomialDistribution.java
@@ -0,0 +1,55 @@
+package vk.dev.trials.hackerrank.ten_days_of_statistics;
+
+import java.util.Scanner;
+
+/**
+ * Day4_BinomialDistribution.
+ *
+ * @author Vladimir_Kuragin
+ */
+public class Day4_BinomialDistribution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(input());
+        double a = Double.parseDouble(sc.next());
+        double b = Double.parseDouble(sc.next());
+        sc.close();
+
+        double p = a / (a + b);
+        double q = b / (a + b);
+        System.out.printf("p = %.5f, q = %.5f%n", p, q);
+
+        int n = 6;
+        int x = 3;
+
+        double sum = 0;
+        for (int i = x; i <= n; i++) {
+            long combinations = combinations(n, i);
+            System.out.printf("Number of combinations for %d of %d = %d%n", i, n, combinations);
+            double probability = probability(p, q, n, combinations, i);
+            sum += probability;
+            System.out.printf("Probability for %d of %d = %.5f%n", i, n, probability);
+        }
+
+        System.out.printf("result = %.3f%n", sum);
+    }
+
+    private static String input() {
+        return "1.09 1\n";
+    }
+
+    private static long combinations(int n, int x) {
+        return factorial(n) / (factorial(x) * factorial(n - x));
+    }
+
+    private static double probability(double p, double q, int n, long combinations, int i) {
+        return combinations * Math.pow(p, i) * Math.pow(q, (n-i));
+    }
+
+    private static long factorial(long x) {
+        if (x <= 1) {
+            return 1;
+        }
+        return x * factorial(x - 1);
+    }
+}
